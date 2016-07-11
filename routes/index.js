e23b3045036f3244aead35e8e40f8b441577829c
phapdinh <centralparkloop@yahoo.com>
2016-07-11 14:53:38 -0400

    allows api to receive query parameters

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,10 @@ var tweets;
 var params = { q: 'water since:2011-07-11', lang: 'en', count: 10 };
 
 router.get('/', function(req, res, next) {
+	var newQ = req.query.q;
+	if(newQ) {
+		params.q = newQ;
+	}
 	bot.get('search/tweets', params, function(err, data, response) {
 		tweets = data.statuses;
 	
