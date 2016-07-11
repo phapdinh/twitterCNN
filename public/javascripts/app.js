273535addcc22471958540346ff2e9700c6d83e0
phapdinh <centralparkloop@yahoo.com>
2016-07-11 16:49:18 -0400

    added a search bar to allow for user input

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,29 +1,8 @@
 function loadData() {
-	var search_term = {
-            q: 'water'
-    };
-
-	$.ajax({
-          /* the 'param' function ensures that our search terms are properly encoded
-             see: http://api.jquery.com/jQuery.param/ */
-        url: 'https://api.twitter.com/1.1/search/tweets.json?' + $.param(search_term),
-          
-          /* the data type should be set to jsonp. for more info on that see:
-             http://en.wikipedia.org/wiki/JSONP */
-        dataType: 'jsonp',
-		success: function(data) {
-		// uncomment the below to see the data in the console
-        console.log(data);
-        /* loop through each item inside of data['results'] and
-            extract the 'text' property.
-            we then use that to create a list item tag (<li>) and append
-            that to the HTML element with id 'tweets'
-        */
-        for (item in data['results']) {
-			$('#tweets').append(
-				'<li>' + data['results'][item]['text'] + '</li>');
-            }
-    }});
+	var input = $('#input-field').val();
+	var url = 'http://localhost:3000/?' + $.param({q: input});
+	console.log(url);
+	window.location.assign(url);
 };
 
 $('#submit').click(loadData);
