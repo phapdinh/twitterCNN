265b223aa79c5afb7e42609cc490879a421fd94a
phapdinh <centralparkloop@yahoo.com>
2016-07-11 12:10:07 -0400

    added ejs and put tweets on template

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,29 @@
-var app = angular.module('groceryApp',[]);
+function loadData() {
+	var search_term = {
+            q: 'water'
+    };
+
+	$.ajax({
+          /* the 'param' function ensures that our search terms are properly encoded
+             see: http://api.jquery.com/jQuery.param/ */
+        url: 'https://api.twitter.com/1.1/search/tweets.json?' + $.param(search_term),
+          
+          /* the data type should be set to jsonp. for more info on that see:
+             http://en.wikipedia.org/wiki/JSONP */
+        dataType: 'jsonp',
+		success: function(data) {
+		// uncomment the below to see the data in the console
+        console.log(data);
+        /* loop through each item inside of data['results'] and
+            extract the 'text' property.
+            we then use that to create a list item tag (<li>) and append
+            that to the HTML element with id 'tweets'
+        */
+        for (item in data['results']) {
+			$('#tweets').append(
+				'<li>' + data['results'][item]['text'] + '</li>');
+            }
+    }});
+};
+
+$('#submit').click(loadData);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+var twit = require('twit');
+
+var bot = new twit({
+	consumer_key: 'n067jv4mhRfRwmzbYCN4aQJRM',
+	consumer_secret: '9wL2KWC1Iyh0UztHMmVX6bqmzEtWUR8TUDQqBn2SUV0Cmv9sDG',
+	access_token: '752327172585586689-B4mBeFtTGdgjAbSlCWQtKBA2A1ZZCO8',
+	access_token_secret: 'IMhPHxaO1ax7zPIwOlfsRsz8Z089fYwSatkyatswLIu9I'
+})
+
+var tweets;
+bot.get('search/tweets', { q: 'water since:2011-07-11', lang: 'en', count: 3 }, function(err, data, response) {
+	tweets = data.statuses;
+	
+	if(err) {
+		console.log('Something went wrong');
+	}
+	
+	for(var i = 0; i < tweets.length; i++) {
+		console.log(tweets[i].text);
+	}
+
+	/* GET home page. */
+	router.get('/', function(req, res, next) {
+		res.render('index', {data: tweets});
+	});
+})
 
 module.exports = router;
