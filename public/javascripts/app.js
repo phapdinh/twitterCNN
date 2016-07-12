09b9a582b769f9f1bb532768211d9e72d3d6e43e
phapdinh <centralparkloop@yahoo.com>
2016-07-12 12:10:50 -0400

    made the search lookup url from location object to make it more dynamic

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,13 @@
 function loadData() {
 	var input = $('#input-field').val();
-	var url = 'http://localhost:3000/?' + $.param({q: input});
-	console.log(url);
+	var url = location.origin + '?' + $.param({q: input});
 	window.location.assign(url);
 };
 
 $('#submit').click(loadData);
 
 function reloadPage() {
-	window.location.reload();
+	window.location.reload(true);
 }
 
 $('#reload').click(reloadPage);
