86aaed3152c2e9d016ee1a576be0e40c0095eda1
phapdinh <centralparkloop@yahoo.com>
2016-07-12 16:05:37 -0400

    deleted twitter keys

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 var twit = require('twit');
 
 var bot = new twit({
-	consumer_key: 'n067jv4mhRfRwmzbYCN4aQJRM',
-	consumer_secret: '9wL2KWC1Iyh0UztHMmVX6bqmzEtWUR8TUDQqBn2SUV0Cmv9sDG',
-	access_token: '752327172585586689-B4mBeFtTGdgjAbSlCWQtKBA2A1ZZCO8',
-	access_token_secret: 'IMhPHxaO1ax7zPIwOlfsRsz8Z089fYwSatkyatswLIu9I'
+	consumer_key: '....',
+	consumer_secret: '....',
+	access_token: '....',
+	access_token_secret: '....'
 })
 
 module.exports = bot;
