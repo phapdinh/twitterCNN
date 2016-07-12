8e4c0c8e30b0badadd47f743b29b4319af6a430d
phapdinh <centralparkloop@yahoo.com>
2016-07-12 15:05:53 -0400

    extracted out bot into its own module

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var bot = require('../bot');
 
-var twit = require('twit');
-
-var bot = new twit({
-	consumer_key: 'n067jv4mhRfRwmzbYCN4aQJRM',
-	consumer_secret: '9wL2KWC1Iyh0UztHMmVX6bqmzEtWUR8TUDQqBn2SUV0Cmv9sDG',
-	access_token: '752327172585586689-B4mBeFtTGdgjAbSlCWQtKBA2A1ZZCO8',
-	access_token_secret: 'IMhPHxaO1ax7zPIwOlfsRsz8Z089fYwSatkyatswLIu9I'
-})
-
-var tweets;
 var params = { q: 'water since:2011-07-11', lang: 'en', count: 10 };
 
 router.get('/', function(req, res, next) {
@@ -19,7 +10,7 @@ router.get('/', function(req, res, next) {
 		params.q = newQ;
 	}
 	bot.get('search/tweets', params, function(err, data, response) {
-		tweets = data.statuses;
+		var tweets = data.statuses;
 	
 		if(err) {
 			console.log('Something went wrong');
