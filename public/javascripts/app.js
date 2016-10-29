6e26ca14c3c1a44a65f8a96beef304ee7fcb77ff
phapdinh <centralparkloop@yahoo.com>
2016-10-29 01:19:56 -0400

    added jshint to webpack config

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,11 +1,11 @@
-var $ = require("../../bower_components/jquery/dist/jquery.min.js")
+var $ = require("../../bower_components/jquery/dist/jquery.min.js");
 require("../stylesheets/style.css");
 
 function loadData() {
 	var input = $('#input-field').val();
 	var url = location.origin + '?' + $.param({q: input});
 	window.location.assign(url);
-};
+};\
 
 $('#submit').click(loadData);
 $('#input-field').keypress(function(e) {
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,13 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
+		preLoaders: [
+            {
+                test: /app.js/g, // include .js files
+                exclude: /node_modules/, // exclude any and all files in the node_modules folder
+                loader: "jshint-loader"
+            }
+        ],
         loaders: [
             { test: /\.css$/, loader: "style!css" }
         ]
